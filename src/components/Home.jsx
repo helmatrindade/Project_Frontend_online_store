448bf7ab0c44b54b691505487e0fd424484391ed
Wallace Rodrigo <wallacerodrigo2004@gmail.com>
2023-01-11 17:45:45 -0300

    requisito 4

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,36 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { getCategories } from '../services/api';
 
 class Home extends Component {
+  state = {
+    apiReturn: [],
+  };
+
+  async componentDidMount() {
+    const apiReturn = await getCategories();
+    this.setState({ apiReturn });
+  }
+
   render() {
+    const { apiReturn } = this.state;
+
     return (
       <div>
         <Link data-testid="shopping-cart-button" to="cart">Carrinho</Link>
         <h2 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h2>
+        <div className="categories">
+          {
+            apiReturn.map((category) => (
+              <label data-testid="category" key={ category.id } htmlFor="categories">
+                { category.name }
+                <input type="radio" name="categories" id="" />
+              </label>
+            ))
+          }
+        </div>
       </div>
     );
   }
